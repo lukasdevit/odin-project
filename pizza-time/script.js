2ad543925a24934c9fa906b238429a7c8a3964af
Lukasz Goletz <lukaszgoletz@gmail.com>
2023-09-24 02:55:34 +0200

    Sauce handler and more Json data
    Added sauce input handler (basic).
    Implemented json data for cheeses and toppings.

--- pizza-time/script.js
+++ pizza-time/script.js
@@ -8,15 +8,14 @@ fetch("./data.json")
     console.log(jsonData);
     console.log(jsonData.doughTypes);
     addDoughSelectors();
+    addSauceSelectors();
   })
   .catch((error) => console.log(error));
-
 
 // html consts
 const doughImage = document.getElementById("doughImage");
 const doughType = document.getElementById("doughTypes");
 // end of html consts
-
 
 // import data and set html selectors
 function addDoughSelectors() {
@@ -27,6 +26,18 @@ function addDoughSelectors() {
       doughList.innerHTML += `<option value="${dough.id}" selected disabled>${dough.name}</option>`;
     } else {
       doughList.innerHTML += `<option value="${dough.id}">${dough.name}</option>`;
+    }
+  });
+}
+
+function addSauceSelectors() {
+  let sauceList = document.getElementById("sauceTypes");
+  sauceList.innerHTML = "";
+  jsonData.souces.forEach((sauce) => {
+    if (sauce.id === "placeholder") {
+      sauceList.innerHTML += `<option value="${sauce.id}" selected disabled>${sauce.name}</option>`;
+    } else {
+      sauceList.innerHTML += `<option value="${sauce.id}">${sauce.name}</option>`;
     }
   });
 }
@@ -45,4 +56,18 @@ function changeDough() {
   });
 }
 
+function changeSauce() {
+  sauceTypes.addEventListener("change", () => {
+    const selectedSauceId = sauceTypes.value;
+    const selectedSauce = jsonData.souces.find(
+      (sauce) => sauce.id === selectedSauceId
+    );
+
+    if (selectedSauce) {
+      console.log("Sauce type: " + selectedSauce.name);
+    }
+  });
+}
+
 changeDough();
+changeSauce();
